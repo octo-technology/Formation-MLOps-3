9853b63b215358e79df1f00a892358012e4bc133
DAMU <damu@octo.com>
2023-06-15 17:18:10 +0200

    feat: add validator for failure reporting

--- source/domain/entities/customer_data_handler.py
+++ source/domain/entities/customer_data_handler.py
@@ -1,6 +1,5 @@
 import pandera as pa
 
-
 CUSTOMER_NAME_COLUMN_NAME = "name"
 CUSTOMER_AGE_COLUMN_NAME = "age"
 CUSTOMER_EDUCATION_COLUMN_NAME = "education"
@@ -32,7 +31,7 @@ class RawCustomerSchema(pa.DataFrameModel):
 
     # TODO: Retirer pour l'exemple du TP
     # TODO: Ajouter le type pour spending préciser le type
-    # TODO: Ajouter un check pour avoir une valeur comprise entre 0 et 1
+    # TODO: Ajouter un check pour vérifier si la valeur est comprise entre 0 et 1
     purchase_frequency: pa.typing.Series[pa.typing.Float64] = pa.Field(
         alias=CUSTOMER_PURCHASE_FREQUENCY_COLUMN_NAME, coerce=True
     )
@@ -42,5 +41,7 @@ class RawCustomerSchema(pa.DataFrameModel):
     )
 
     @pa.check("education", name="is_education_valid")
-    def is_education_valid(cls, education: pa.typing.Series[int]) -> pa.typing.Series[bool]:
+    def is_education_valid(
+        cls, education: pa.typing.Series[int]
+    ) -> pa.typing.Series[bool]:
         return education in CUSTOMER_EDUCATION_VALUES

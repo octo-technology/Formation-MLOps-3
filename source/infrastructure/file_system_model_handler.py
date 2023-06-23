bc29d959be767f55163570d92987f4f6314505b8
TOUL <toul>
2023-06-23 16:17:17 +0200

    Load model from MLFlow and fix ci

--- source/infrastructure/file_system_model_handler.py
+++ source/infrastructure/file_system_model_handler.py
@@ -1,6 +1,8 @@
 import os
 
 import joblib
+import mlflow
+from mlflow import MlflowException
 
 from models import MODEL_REGISTRY
 from source.domain.port.model_handler import ModelHandler
@@ -13,4 +15,7 @@ class FilSystemModelHandler(ModelHandler):
         joblib.dump(model, MODEL_PATH)
 
     def load_model(self):
-        return joblib.load(MODEL_PATH)
+        try:
+            return mlflow.pyfunc.load_model(model_uri=f"models:/customer/production")
+        except MlflowException:
+            return joblib.load(MODEL_PATH)

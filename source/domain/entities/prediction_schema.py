5031af39793bfa3628bd9601d34d52d674a0cd92
TOUL <toul>
2023-06-23 16:22:48 +0200

    Prepare TP5

--- source/domain/entities/prediction_schema.py
+++ source/domain/entities/prediction_schema.py
@@ -5,9 +5,3 @@ class PredictionSchema(pa.DataFrameModel):
     inference: pa.typing.Series[pa.typing.Float64] = pa.Field(
         alias="inference"
     )
-
-    @pa.check("inference", name="is_inference_valid")
-    def is_education_valid(
-            cls, inference: pa.typing.Series[pa.typing.Float64]
-    ) -> pa.typing.Series[bool]:
-        return inference.between(0., 100000)

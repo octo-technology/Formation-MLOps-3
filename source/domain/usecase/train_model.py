bd3891e3eec9c48b981dadbff2f4a2308c904df0
TOUL <toul>
2023-06-23 16:19:31 +0200

    Create TP7

--- source/domain/usecase/train_model.py
+++ source/domain/usecase/train_model.py
@@ -1,26 +1,18 @@
-import os.path
+import os
 
 import mlflow.sklearn
 import pandas as pd
 import pandera as pa
-from codecarbon import track_emissions
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 from data import DATA_PATH
-from models import MODEL_REGISTRY
 from source.domain.entities.customer_columns import DataSetColumns, EDUCATION_LEVEL
 from source.domain.entities.customer_data_schema import RawCustomerSchema
 from source.domain.port.model_handler import ModelHandler
 
-MODEL_PATH = os.path.join(MODEL_REGISTRY, 'model.joblib')
 
-
-# TODO: [TP3] Retirer decorateur @pa.check_input(RawCustomerSchema)
-# TODO: [TP3] Ajouter une verification sur l'input en utilisant les checks définis
-# TODO: [TP3] Observer l'erreur, ici le check génère une erreur pour spending > 1
-# TODO: [TP3] Utiliser la methode validate pour filtrer les lignes qui ne passent pas les checks et logger ces lignes
 @pa.check_input(RawCustomerSchema)
 def prepare_data(raw_customer_df: pa.typing.DataFrame[RawCustomerSchema]):
     raw_customer_df = raw_customer_df.merge(EDUCATION_LEVEL, on=DataSetColumns.education).drop(
@@ -28,7 +20,6 @@ def prepare_data(raw_customer_df: pa.typing.DataFrame[RawCustomerSchema]):
     return raw_customer_df
 
 
-@track_emissions(project_name='Train model', offline=True, country_iso_code='FRA')
 def train_model(model_handler: ModelHandler):
     df = pd.read_csv(os.path.join(DATA_PATH, 'customer_data.csv'), sep=',')
     X_train, X_test, y_train, y_test = train_test_split(

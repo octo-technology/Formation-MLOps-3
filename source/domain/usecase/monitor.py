f7d5355d7fecc12b7a399b84f459bef7adf9f200
TOUL <toul>
2023-06-23 11:14:47 +0200

    Add tz

--- source/domain/usecase/monitor.py
+++ source/domain/usecase/monitor.py
@@ -1,6 +1,7 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 import pandas as pd
+import pytz
 
 from source.domain.port.monitoring_handler import MonitoringHandler
 
@@ -10,5 +11,5 @@ INFERENCE_COL = 'inference'
 
 def monitor(df: pd.DataFrame, inference: pd.Series, monitoring_handler: MonitoringHandler):
     df[INFERENCE_COL] = inference
-    df[DATETIME_COL] = datetime.now().replace(tzinfo=timezone.utc)
+    df[DATETIME_COL] = datetime.now().astimezone(pytz.UTC)
     monitoring_handler.save_inference(inference_df=df)
